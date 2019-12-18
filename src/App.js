ceaa52c0617e716c28e0a91c765fb330d9d963ae
KSentak <k.sentak@outlook.com>
2019-12-18 12:35:34 -0500

    Continued stylizing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Wrapper from "./components/Wrapper";
 import CharacterCard from "./components/CharacterCard";
@@ -43,19 +42,14 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React Clicky Game!!</h1>
-				</header>
-				<h3 className="App-intro">
+				<h3 className="directions text-white">
 					<p>Click on one character to begin. Remember who you click and try to get all 12 characters in a row without repeating a pick. Your high score will be logged.</p>
-					<p className="score"><strong>Score: {this.state.score} | TopScore: {this.state.topScore}</strong></p>
+					<p className="score"><strong>Score: {this.state.score} | High Score: {this.state.topScore}</strong></p>
 					<p className="message"><strong>{this.state.message}</strong></p>
 				</h3>
 				<Wrapper
-					shakeWrapper={this.state.shakeit}
-					pictures=
-					{this.state.characters.map(item => (
+					shakeWrapper = {this.state.shakeit}
+					pictures = {this.state.characters.map(item => (
 						<CharacterCard
 							key={item.id}
 							id={item.id}
