e1a2952ae9e05941eca4850231081b8ddafc3904
KSentak <k.sentak@outlook.com>
2019-12-18 23:56:13 -0500

    Updates

---
+++ precache-manifest.66080973a6e0368ff54704cbbaaa685d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b407e6a76c17681bb5d33d04251707fa",
+    "url": "/ks-memory-game/index.html"
+  },
+  {
+    "revision": "17a435d999efce6674eb",
+    "url": "/ks-memory-game/static/css/main.5539025f.chunk.css"
+  },
+  {
+    "revision": "5d8709765a4f433a0822",
+    "url": "/ks-memory-game/static/js/2.87d9fc73.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ks-memory-game/static/js/2.87d9fc73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17a435d999efce6674eb",
+    "url": "/ks-memory-game/static/js/main.8090b101.chunk.js"
+  },
+  {
+    "revision": "eecd753bd41b20a36abd",
+    "url": "/ks-memory-game/static/js/runtime-main.5ae4336c.js"
+  }
+]);
